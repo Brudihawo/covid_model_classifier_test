a0a3d40cfb1aa0c37fba979efa8ec1129252e0fe
brudihawo <hawo.hoefer98@freenet.de>
2021-09-06 10:29:30 +0200

    improved download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,1 +1,5 @@
-wget -O ../data/coviddata.csv https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data
+filepath="$(dirname ${BASH_SOURCE[0]})/../data"
+
+mkdir -p $filepath
+
+wget -O $filepath/coviddata.csv https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data
